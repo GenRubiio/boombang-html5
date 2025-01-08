5f791659b0a82bef94b9d832493e1d4b98c884c0
evgeny.lyubeznyy <evgeny.lyubeznyy@basetis.com>
2025-01-08 08:43:08 +0100

    Add werewolf animation configuration and enum for avatar animations

---
+++ client/src/phaser/enums/AvatarAnimationsEnum.js
@@ -0,0 +1,26 @@
+import Enum from './Enum';
+
+const AvatarAnimationsEnum = new Enum({
+    UP_WALK: 'up_walk',
+    UP_IDLE: 'up_idle',
+    LEFTUP_WALK: 'leftup_walk',
+    LEFTUP_IDLE: 'leftup_idle',
+    RIGHTUP_WALK: 'rightup_walk',
+    RIGHTUP_IDLE: 'rightup_idle',
+    LEFT_WALK: 'left_walk',
+    LEFT_IDLE: 'left_idle',
+    RIGHT_WALK: 'right_walk',
+    RIGHT_IDLE: 'right_idle',
+    DOWN_WALK: 'down_walk',
+    DOWN_IDLE: 'down_idle',
+    LEFTDOWN_WALK: 'leftdown_walk',
+    LEFTDOWN_IDLE: 'leftdown_idle',
+    RIGHTDOWN_WALK: 'rightdown_walk',
+    RIGHTDOWN_IDLE: 'rightdown_idle',
+    RIGHT_PUNCH_DOY: 'right_punch_doy',
+    LEFT_PUNCH_DOY: 'left_punch_doy',
+    RIGHT_PUNCH_REC: 'right_punch_rec',
+    LEFT_PUNCH_REC: 'left_punch_rec',
+});
+
+export default AvatarAnimationsEnum;
