d83fa863fdf94cfb0d7c944b28a16a9f0e1f71b3
evgeny.lyubeznyy <evgeny.lyubeznyy@basetis.com>
2025-01-08 09:17:29 +0100

    Add werewolf animation configuration and update AvatarAnimationsLoad to utilize it

--- client/src/phaser/load/AvatarAnimationsLoad.js
+++ client/src/phaser/load/AvatarAnimationsLoad.js
@@ -1,7 +1,12 @@
+import werewolf_config from "../../assets/game/avatars/1/werewolf-config.json";
+import AvatarsEnum from "../enums/AvatarsEnum";
 
 class AvatarAnimationsLoad {
     static main(gameScene) {
+        this.loadAvatarAnimations(gameScene, werewolf_config, AvatarsEnum.WEREWOLF);
+    }
 
+    static loadAvatarAnimations(gameScene, config, avatarId) {
     }
 }
 
