87e5bc8d9638651096639fae3a238fa167b5d401
evgeny.lyubeznyy <evgeny.lyubeznyy@basetis.com>
2025-01-31 14:59:12 +0100

    Initialize Electron app with main process and basic configuration; add .gitignore and package.json

---
+++ launcher/main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow, globalShortcut } = require('electron');
+
+let mainWindow;
+
+app.whenReady().then(() => {
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+
+  // Cargar la URL donde está el frontend de Vue
+  const vueURL = "http://localhost:5173"; // Cambia esto si tu Vue está en otro servidor
+  mainWindow.loadURL(vueURL);
+
+  // Registrar atajo de teclado F5 para recargar la página
+  globalShortcut.register('F5', () => {
+    if (mainWindow) mainWindow.reload();
+  });
+
+  // Registrar Ctrl+R para recargar (Mac usa ⌘+R)
+  globalShortcut.register('CommandOrControl+R', () => {
+    if (mainWindow) mainWindow.reload();
+  });
+});
+
+app.on('will-quit', () => {
+  globalShortcut.unregisterAll(); // Eliminar los atajos al cerrar la app
+});
