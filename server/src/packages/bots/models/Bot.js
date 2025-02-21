5a855729cba869a2ea8d9b27aaa77f7a8d7c4389
evgeny.lyubeznyy <evgeny.lyubeznyy@basetis.com>
2025-02-21 07:51:32 +0100

    Refactor Bot model to use enums for socket events; improve user selection logic and error handling

--- server/src/packages/bots/models/Bot.js
+++ server/src/packages/bots/models/Bot.js
@@ -1,5 +1,7 @@
 const { io } = require("socket.io-client");
 require('dotenv').config();
+const RequestSocketsEnum = require('../../../enums/RequestSocketsEnum');
+const ResponseSocketsEnum = require('../../../enums/ResponseSocketsEnum');
 
 class Bot {
     constructor(username, password) {
@@ -9,28 +11,55 @@ class Bot {
         this.uppercutInterval = null;
 
         this.socket.on("connect", () => {
-            console.log(`${this.username} conectado como bot.`);
+            //console.log(`${this.username} conectado como bot.`);
             this.login();
         });
 
         this.socket.on("login_success", (data) => {
             setInterval(() => {
-                this.joinArea();
-            }, 3000);
+                this.joinArea(1);
+            }, 1000);
 
             this.startActions();
             this.selectUser();
         });
 
-        this.socket.on("response:get_public_area_users", (data) => {
+        this.socket.on(ResponseSocketsEnum.GET_PUBLIC_AREA_USERS, (data) => {
             clearInterval(this.uppercutInterval);
-            const randomUser = data.players[Math.floor(Math.random() * data.players.length)];
-            if (randomUser.id !== this.socket.id) {
-                this.socket.emit('request:user_select_user', {
-                    socketId: randomUser.id
+            // Encuentra la información del jugador local (el que hace la petición)
+            const localUser = data.players.find((player) => player.id === this.socket.id);
+            if (!localUser) return; // si no se encontró el jugador local, salimos
+
+            // Función auxiliar para calcular la distancia entre dos jugadores
+            // asumiendo que cada player tiene x, y, z.
+            const distancia = (p1, p2) => {
+                const dx = p1.x - p2.x;
+                const dy = p1.y - p2.y;
+                const dz = p1.z - p2.z;
+                return Math.sqrt(dx * dx + dy * dy + dz * dz);
+            };
+
+            // Filtramos al jugador local y reducimos para encontrar al más cercano
+            let usuarioMasCercano = null;
+            let menorDistancia = Infinity;
+
+            data.players
+                .filter((player) => player.id !== this.socket.id)
+                .forEach((player) => {
+                    const dist = distancia(localUser, player);
+                    if (dist < menorDistancia) {
+                        menorDistancia = dist;
+                        usuarioMasCercano = player;
+                    }
+                });
+
+            // Si encontramos a un usuario más cercano, lanzamos la acción
+            if (usuarioMasCercano) {
+                this.socket.emit(RequestSocketsEnum.USER_SELECT_USER, {
+                    socketId: usuarioMasCercano.id
                 });
                 this.uppercutInterval = setInterval(() => {
-                    this.socket.emit('request:send_uppercut');
+                    this.socket.emit(RequestSocketsEnum.SEND_UPPERCUT);
                 }, 100);
             }
         });
@@ -38,11 +67,15 @@ class Bot {
         this.socket.on("disconnect", () => {
             console.log(`${this.username} desconectado.`);
         });
+
+        this.socket.on("error_critical", () => {
+            this.socket.disconnect();
+        });
     }
 
     selectUser() {
         setInterval(() => {
-            this.socket.emit("request:get_public_area_users", {});
+            this.socket.emit(RequestSocketsEnum.GET_PUBLIC_AREA_USERS, {});
         }, 10000);
     }
 
@@ -53,8 +86,7 @@ class Bot {
     moveRandomly() {
         const x = Math.floor(Math.random() * 30);
         const y = Math.floor(Math.random() * 30);
-        this.socket.emit("request:user_move", { x: x, y: y });
-        //console.log(`Moviendo a ${this.username} a (${x}, ${y})`);
+        this.socket.emit(RequestSocketsEnum.USER_MOVE, { x: x, y: y });
     }
 
     startActions() {
@@ -63,8 +95,8 @@ class Bot {
         }, 3000); // Se mueve cada 3 segundos
     }
 
-    joinArea() {
-        this.socket.emit("request:join_public_area", { areaId: 1 });
+    joinArea(areaId) {
+        this.socket.emit(RequestSocketsEnum.JOIN_PUBLIC_AREA, { areaId: areaId });
     }
 }
 
