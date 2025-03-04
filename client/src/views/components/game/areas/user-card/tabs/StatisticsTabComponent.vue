f8ccd4484a20940ee863d3142581e56894294177
evgeny.lyubeznyy <evgeny.lyubeznyy@basetis.com>
2025-03-04 07:45:18 +0100

    Add uppercuts statistics display to StatisticsTabComponent

--- client/src/views/components/game/areas/user-card/tabs/StatisticsTabComponent.vue
+++ client/src/views/components/game/areas/user-card/tabs/StatisticsTabComponent.vue
@@ -1,11 +1,34 @@
 <template>
-  <div class="container"></div>
+  <div class="container">
+    <div class="container__uppers-data">
+      <div class="container__uppers-data__title">Uppercuts</div>
+      <div class="container__uppers-data__data-container">
+        <div class="container__uppers-data__data-container__count">1000</div>
+        <div class="container__uppers-data__data-container__title">
+          Enviados
+        </div>
+      </div>
+      <div class="container__uppers-data__data-container">
+        <div class="container__uppers-data__data-container__count">1000</div>
+        <div class="container__uppers-data__data-container__title">
+          Recibidos
+        </div>
+      </div>
+    </div>
+    <div class="container__uppers-right">
+      <img :src="red_upper" alt="upper" />
+    </div>
+  </div>
 </template>
 
 <script>
+import red_upper from "../../../../../../assets/game/ficha/uppercuts/red.png";
+
 export default {
   data() {
-    return {};
+    return {
+      red_upper,
+    };
   },
   methods: {},
   computed: {},
@@ -22,5 +45,55 @@ export default {
   height: 90px;
   position: relative;
   z-index: 0;
+  display: flex;
+}
+
+.container__uppers-data {
+  padding: 8px;
+  width: 120px;
+}
+
+.container__uppers-right{
+  padding-top: 15px;
+}
+
+.container__uppers-data__title {
+  font-size: 18px;
+  font-weight: bold;
+  color: white;
+  background-color: #005491;
+  padding: 0 3px;
+  border-radius: 5px;
+  text-align: start;
+}
+
+.container__uppers-data__data-container{
+  display: flex;
+  justify-content: space-between;
+  margin-top: 5px;
+}
+
+.container__uppers-data__data-container__count{
+  font-size: 11px;
+  font-weight: bold;
+  color: white;
+  background-color: #005491;
+  padding: 0 5px;
+  border-radius: 5px;
+  text-align: center;
+  height: 17px;
+  width: 33px;
+}
+
+.container__uppers-data__data-container__title{
+  font-size: 11px;
+  font-weight: bold;
+  color: white;
+  background-color: #005491;
+  padding: 0 5px;
+  border-radius: 5px;
+  text-align: start;
+  height: 17px;
+  width: 63px;
 }
 </style>
