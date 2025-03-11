45cea489323fa03aa42c3f48f039ed4dda2cb8a9
evgeny.lyubeznyy <evgeny.lyubeznyy@basetis.com>
2025-03-11 09:11:38 +0100

    Add new assets and enhance LobbyScreen layout with background and avatar

--- client/src/views/screens/game/LobbyScreen.vue
+++ client/src/views/screens/game/LobbyScreen.vue
@@ -1,9 +1,26 @@
 <template>
   <div id="lobby">
-    <h1>Lobby</h1>
-    <div v-for="area in areas" :key="area.id" class="room">
-      <p>{{ area.name }} ({{ area.total_users_in }})</p>
-      <button @click="joinRoom(area.id)">Join Room</button>
+    <div class="lobby__background">
+      <img :src="background" alt="background" />
+    </div>
+    <div class="lobby__flor">
+      <img :src="flor" alt="flor" />
+    </div>
+    <div class="lobby__foreground">
+      <img :src="foreground" alt="foreground" />
+    </div>
+    <div class="lobby__marikita">
+      <img :src="marikita" alt="marikita" />
+    </div>
+    <div class="lobby__avatar">
+      <img :src="avatar" alt="avatar" />
+    </div>
+    <div class="lobby__rooms">
+      <h1>{{ $socket.user.avatar_id }}</h1>
+      <div v-for="area in areas" :key="area.id" class="room">
+        <p>{{ area.name }} ({{ area.total_users_in }})</p>
+        <button @click="joinRoom(area.id)">Join Room</button>
+      </div>
     </div>
   </div>
 </template>
@@ -12,17 +29,24 @@
 import socket from "../../../sockets/socket";
 import RequestSocketsEnum from "../../../enums/RequestSocketsEnum";
 import ResponseSocketsEnum from "../../../enums/ResponseSocketsEnum";
+import background from "../../../assets/game/lobby/background.png";
+import flor from "../../../assets/game/lobby/flor.webp";
+import foreground from "../../../assets/game/lobby/foreground.png";
+import marikita from "../../../assets/game/lobby/marikita.webp";
 
 export default {
   data() {
     return {
       areas: [],
+      background,
+      flor,
+      foreground,
+      marikita,
+      avatar: null,
     };
   },
-  created() {
+  async created() {
     this.$emit("updateLoading", true);
-    // Solicitar las salas al conectarse
-    //console.log("Solicitando salas...");
     socket.emit(RequestSocketsEnum.GET_PUBLIC_AREAS);
 
     socket.off(ResponseSocketsEnum.UPDATE_PUBLIC_AREAS);
@@ -31,6 +55,15 @@ export default {
       this.areas = areas;
       this.$emit("updateLoading", false);
     });
+
+    try {
+      const { default: avatarImage } = await import(
+        /* @vite-ignore */ `../../../assets/game/lobby/avatars/${this.$socket.user.avatar_id}.svg`
+      );
+      this.avatar = avatarImage;
+    } catch (error) {
+      console.error("Error cargando el avatar:", error);
+    }
   },
   components: {},
   methods: {
@@ -52,9 +85,55 @@ export default {
 
 <style>
 #lobby {
+  position: relative;
   background-color: #f0f0f0;
   width: 100%;
   height: 100%;
   overflow: hidden;
+}
+
+.lobby__background img {
+  position: absolute;
+  top: -22px;
+  left: 0;
+  z-index: 0;
+}
+
+.lobby__flor img {
+  position: absolute;
+  top: 0px;
+  left: 35px;
+  z-index: 0;
+}
+
+.lobby__marikita img {
+  position: absolute;
+  top: 395px;
+  left: -222px;
+  z-index: 0;
+}
+
+.lobby__foreground img {
+  position: absolute;
+  top: 227px;
+  left: 0px;
+  z-index: 0;
+}
+
+.lobby__rooms {
+  position: relative;
+  z-index: 1;
+  background-color: black;
+  color: white;
+  padding: 10px;
+  border-radius: 10px;
+  width: 200px;
+}
+
+.lobby__avatar img {
+  position: absolute;
+  top: 266px;
+  left: 455px;
+  z-index: 1;
 }
 </style>
