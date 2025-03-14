10aef459dfa1f2acc71ff408fce8f0990a186a39
evgeny.lyubeznyy <evgeny.lyubeznyy@basetis.com>
2025-03-14 08:02:21 +0100

    Add public scene management features including migrations, seeders, and routes

---
+++ api/app/Models/PublicScene.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Models;
+
+use Backpack\CRUD\app\Models\Traits\CrudTrait;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PublicScene extends Model
+{
+    use CrudTrait;
+    use HasFactory;
+
+    /*
+    |--------------------------------------------------------------------------
+    | GLOBAL VARIABLES
+    |--------------------------------------------------------------------------
+    */
+
+    protected $table = 'public_scenes';
+    // protected $primaryKey = 'id';
+    // public $timestamps = false;
+    protected $guarded = ['id'];
+    protected $fillable = [
+        'name',
+        'type',
+        'max_users',
+        'map_width',
+        'map_height',
+        'map',
+        'start_x',
+        'start_y',
+        'start_z',
+        'parent_id',
+        'lft',
+        'rgt',
+        'depth',
+        'active',
+    ];
+    // protected $hidden = [];
+
+    /*
+    |--------------------------------------------------------------------------
+    | FUNCTIONS
+    |--------------------------------------------------------------------------
+    */
+
+    /*
+    |--------------------------------------------------------------------------
+    | RELATIONS
+    |--------------------------------------------------------------------------
+    */
+
+    /*
+    |--------------------------------------------------------------------------
+    | SCOPES
+    |--------------------------------------------------------------------------
+    */
+
+    public function scopeActive($query)
+    {
+        return $query->where($this->table . '.active', 1);
+    }
+
+    public function scopeOrdered($query)
+    {
+        return $query->orderBy($this->table . '.lft', 'asc')->orderBy($this->table . '.id', 'desc');
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | ACCESSORS
+    |--------------------------------------------------------------------------
+    */
+
+    /*
+    |--------------------------------------------------------------------------
+    | MUTATORS
+    |--------------------------------------------------------------------------
+    */
+}
---
+++ api/database/migrations/2025_03_14_061615_create_table_public_scenes_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('public_scenes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('type');
+            $table->integer('max_users')->default(10);
+            $table->integer('map_width')->default(30);
+            $table->integer('map_height')->default(30);
+            $table->text('map');
+            $table->integer('start_x')->default(11);
+            $table->integer('start_y')->default(11);
+            $table->integer('start_z')->default(2);
+            $table->integer('parent_id')->unsigned()->nullable();
+            $table->integer('lft')->unsigned()->nullable();
+            $table->integer('rgt')->unsigned()->nullable();
+            $table->integer('depth')->unsigned()->nullable();
+            $table->boolean('active')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('public_scenes');
+    }
+};
