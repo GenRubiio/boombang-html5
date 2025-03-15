aeffe36eca10da6882d166bb37be144ca43a38aa
evgeny.lyubeznyy <evgeny.lyubeznyy@basetis.com>
2025-03-15 13:48:52 +0100

    Add Laravel Passport for API authentication with necessary migrations and configuration

--- api/app/Models/User.php
+++ api/app/Models/User.php
@@ -7,12 +7,14 @@ use Spatie\Permission\Traits\HasRoles;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
     use CrudTrait;
     use HasRoles;
     use HasFactory;
+    use HasApiTokens
     use Notifiable;
 
     protected $fillable = [
