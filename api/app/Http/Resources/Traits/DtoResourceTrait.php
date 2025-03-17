118cfa89d762d95726bb06f198cf673c2d830e37
evgeny.lyubeznyy <evgeny.lyubeznyy@basetis.com>
2025-03-17 07:05:07 +0100

    Refactor LoginApiController to use UserResource and implement DtoResourceTrait for user data transformation

---
+++ api/app/Http/Resources/Traits/DtoResourceTrait.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources\Traits;
+
+use Illuminate\Http\Request;
+
+trait DtoResourceTrait
+{
+    public function toDTO(): ?object
+    {
+        return !is_null(self::$data) ? (object)self::toArray(new Request(self::$data->toArray())) : null;
+    }
+
+    public static function collectionToDTO($data)
+    {
+        if (is_null($data)) {
+            return null;
+        }
+        $currentClass = __CLASS__;
+        return $data->map(fn ($object): object => (new $currentClass($object))->toDTO());
+    }
+}
