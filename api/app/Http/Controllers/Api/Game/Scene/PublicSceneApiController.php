d7cf1b70af9cec7eeb1c8668edcfd6ae0bc1faa3
evgeny.lyubeznyy <evgeny.lyubeznyy@basetis.com>
2025-03-17 09:31:11 +0100

    Add PublicSceneApiController and PublicSceneResource for scene retrieval

---
+++ api/app/Http/Controllers/Api/Game/Scene/PublicSceneApiController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Api\Game\Scene;
+
+use Exception;
+use App\Models\PublicScene;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\PublicSceneResource;
+use Illuminate\Http\Resources\Json\JsonResource;
+use App\Http\Controllers\Api\Traits\ResponseApiControllerTrait;
+
+class PublicSceneApiController extends Controller
+{
+    use ResponseApiControllerTrait;
+
+    public function get(Request $request): JsonResource
+    {
+        try {
+            $items = PublicScene::all();
+            return $this->successResponse(
+                [
+                    'scenes' => PublicSceneResource::collection($items)
+                ]
+            );
+        } catch (Exception $e) {
+            return $this->handleException($e);
+        }
+    }
+}
