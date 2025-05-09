11f318ce05269aabbfdb2ec6c6fb26380bc068cf
evgeny.lyubeznyy <evgeny.lyubeznyy@basetis.com>
2025-05-09 07:29:40 +0200

    feat: Add new minigame scene assets and update preload logic for background image

--- client/src/phaser/preloaders/minigame-scene/GoldenRingPreload.js
+++ client/src/phaser/preloaders/minigame-scene/GoldenRingPreload.js
@@ -1,12 +1,8 @@
-import asset_backgroundImage from "../../../assets/game/scenarios/1/background.webp";
-import asset_item1Image from "../../../assets/game/scenarios/1/item_1.webp";
-import asset_item2Image from "../../../assets/game/scenarios/1/item_2.webp";
+import asset_backgroundImage from "../../../assets/game/minigame-scenes/1/background.webp";
 
 class GoldenRingPreload {
     static main(gameScene) {
         gameScene.load.image("background", asset_backgroundImage);
-        gameScene.load.image("item_1", asset_item1Image);
-        gameScene.load.image("item_2", asset_item2Image);
     }
 }
 
