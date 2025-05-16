ec7aa08ae2ffc38b8550b3e56d018bb32635c13a
evgeny.lyubeznyy <evgeny.lyubeznyy@basetis.com>
2025-05-16 13:16:17 +0200

    feat: Implement RemovePhaserSocketsUtil for streamlined socket event removal in MinigameScene and PublicScene

---
+++ client/src/utils/RemovePhaserSocketsUtil.js
@@ -0,0 +1,37 @@
+
+import ResponseSocketsEnum from '../enums/ResponseSocketsEnum';
+import RequestSocketsEnum from '../enums/RequestSocketsEnum';
+
+class RemovePhaserSocketsUtil {
+    static main(socket) {
+        // Listas de exclusión
+        const excludedResponseEvents = [
+            ResponseSocketsEnum.UPDATE_PUBLIC_AREAS,
+            ResponseSocketsEnum.JOIN_PUBLIC_AREA,
+            ResponseSocketsEnum.MINIGAME_JOIN,
+            ResponseSocketsEnum.MINIGAME_CALL_NOTIFICATION
+        ];
+
+        const excludedRequestEvents = [
+            RequestSocketsEnum.GET_PUBLIC_AREAS,
+            RequestSocketsEnum.JOIN_PUBLIC_AREA
+        ];
+
+        // Remover solo eventos que no están en las listas de exclusión
+        Object.values(ResponseSocketsEnum).forEach((event) => {
+            if (!excludedResponseEvents.includes(event)) {
+                //console.log(`Eliminando evento de respuesta de socket: ${event}`);
+                socket.off(event);
+            }
+        });
+
+        Object.values(RequestSocketsEnum).forEach((event) => {
+            if (!excludedRequestEvents.includes(event)) {
+                //console.log(`Eliminando evento de solicitud de socket: ${event}`);
+                socket.off(event);
+            }
+        });
+    }
+}
+
+export default RemovePhaserSocketsUtil;
