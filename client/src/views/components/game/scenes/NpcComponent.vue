40d7de64e2c137ba5e85a081782595127a24779d
evgeny.lyubeznyy <evgeny.lyubeznyy@basetis.com>
2025-05-18 13:45:25 +0200

    feat: Enhance NPC modal with RingNpcModalComponent; add content and styling for combat participation

--- client/src/views/components/game/scenes/NpcComponent.vue
+++ client/src/views/components/game/scenes/NpcComponent.vue
@@ -2,41 +2,56 @@
   <div class="modal-overlay">
     <div class="modal-content">
       <button class="close-btn" @click="$emit('close')">×</button>
-      <!-- aquí va tu contenido del NPC -->
-      <h2>Información del NPC</h2>
-      <!-- usa props.npcData si lo necesitas -->
+      <RingNpcModalComponent v-if="npcId == NpcEnum.WISE_RING" />
     </div>
   </div>
 </template>
 
 <script>
+import { defineAsyncComponent } from "vue";
+import NpcEnum from "../../../../enums/NpcEnum.js";
+
 export default {
   props: {
-    npcId: Number, // opcional
+    npcId: {
+      type: Number,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      NpcEnum,
+    };
+  },
+  components: {
+    RingNpcModalComponent: defineAsyncComponent(() =>
+      import("../scenes/npc/RingNpcModalComponent.vue")
+    ),
   },
 };
 </script>
 
 <style>
 .modal-overlay {
-  position: fixed;
-  inset: 0;
-  background: rgba(0, 0, 0, 0.6);
   display: flex;
-  justify-content: center;
+  justify-content: end;
   align-items: center;
+  margin: 5px;
   pointer-events: auto;
 }
 .modal-content {
-  background: white;
-  padding: 1.5rem;
+  width: 336px;
+  height: 450px;
+  background: #fffffffa;
+  padding: 10px;
   border-radius: 0.5rem;
   position: relative;
+  user-select: none;
 }
 .close-btn {
   position: absolute;
-  top: 0.5rem;
-  right: 0.5rem;
+  top: 0px;
+  right: 0px;
   font-size: 1.2rem;
   background: none;
   border: none;
