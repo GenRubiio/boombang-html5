47b076c0967b16536d29fa51f0cf8d3467b97b24
evgeny.lyubeznyy <evgeny.lyubeznyy@basetis.com>
2025-05-21 08:04:50 +0200

    feat: Add UppercutsEnum for uppercut levels; update UserModel and UserResource to incorporate uppercut logic

---
+++ server/src/enums/UppercutsEnum.js
@@ -0,0 +1,16 @@
+const Enum = require('./Enum');
+
+const UppercutsEnum = new Enum({
+    RED: 0,
+    PINK: 1,
+    ORANGE: 2,
+    GREEN: 3,
+    BLUE: 4,
+    WHITE: 5,
+    PURPLE: 6,
+    BROWN: 7,
+    BLACK: 8,
+    GOLD: 9
+});
+
+module.exports = UppercutsEnum;
--- server/src/models/UserModel.js
+++ server/src/models/UserModel.js
@@ -1,3 +1,4 @@
+const UppercutsEnum = require('../enums/UppercutsEnum');
 class UserModel {
     constructor(row) {
         this.id = row.id.toString();
@@ -12,6 +13,9 @@ class UserModel {
         this.currentAreaPosition = { x: null, y: null, z: null }; // Posición actual del usuario en el área
         this.selectedUser = null; // Usuario seleccionado por el usuario
         this.movementBlocked = false; // Indica si el movimiento está bloqueado
+
+        this.uppercutLevel = this.calculateUppercutLevel(); // Nivel del uppercut
+        this.uppercutSelected = this.uppercutLevel; // Indica si el usuario ha seleccionado un uppercut
 
         this.finalTarget = null; // Destino final del usuario
 
@@ -60,6 +64,30 @@ class UserModel {
         return true;
     }
 
+    calculateUppercutLevel() {
+        const uppercuts = this.uppercutsSend;
+
+        if (uppercuts >= 2000) {
+            return UppercutsEnum.GOLD;
+        } else if (uppercuts >= 1000) {
+            return UppercutsEnum.BLACK;
+        } else if (uppercuts >= 500) {
+            return UppercutsEnum.BROWN;
+        } else if (uppercuts >= 300) {
+            return UppercutsEnum.PURPLE;
+        } else if (uppercuts >= 100) {
+            return UppercutsEnum.BLUE;
+        } else if (uppercuts >= 50) {
+            return UppercutsEnum.GREEN;
+        } else if (uppercuts >= 25) {
+            return UppercutsEnum.ORANGE;
+        } else if (uppercuts >= 10) {
+            return UppercutsEnum.PINK;
+        } else {
+            return UppercutsEnum.RED;
+        }
+    }
+
 
     // Método para añadir socket al usuario
     addSocket(socket) {
@@ -72,7 +100,7 @@ class UserModel {
         this.finalTarget = null;
         this.movementBlocked = false;
         this.blockedActions = {};
-        
+
         if (!area) {
             this.setSelectedUser(null);
         }
