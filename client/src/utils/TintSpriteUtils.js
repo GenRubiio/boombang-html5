d5dd8748cb547e8eca07a68ee9e41dd6535d5efe
evgeny.lyubeznyy <evgeny.lyubeznyy@basetis.com>
2025-05-21 08:24:25 +0200

    feat: Enhance TintSpriteUtils to support color updates; modify AddUserController for updated shadow tinting logic

--- client/src/utils/TintSpriteUtils.js
+++ client/src/utils/TintSpriteUtils.js
@@ -1,12 +1,30 @@
 class TintSpriteUtils {
-    static tint(gameScene, sprite, originalColor, newColor) {
+    static tint(gameScene, sprite, originalColor, newColor, update = false) {
         const plugin = gameScene.plugins.get('rexColorReplacePipeline');
-        plugin.add(sprite, {
-            originalColor: originalColor,
-            newColor: newColor,
-            epsilon: 0 // sensibilidad, si tienes matices
-            // epsilon: 0.1 // sensibilidad, si tienes matices
-        });
+        if (update) {
+            let pipeline = plugin.get(sprite)[0];
+            if (pipeline) {
+                // actualiza el color
+                pipeline.setOriginalColor(originalColor);
+                pipeline.setNewColor(newColor);
+            } else {
+                // lo aplica por primera vez
+                plugin.add(sprite, {
+                    originalColor: originalColor,
+                    newColor: newColor,
+                    epsilon: 0 // sensibilidad, si tienes matices
+                    // epsilon: 0.1 // sensibilidad, si tienes matices
+                });
+            }
+        }
+        else {
+            plugin.add(sprite, {
+                originalColor: originalColor,
+                newColor: newColor,
+                epsilon: 0 // sensibilidad, si tienes matices
+                // epsilon: 0.1 // sensibilidad, si tienes matices
+            });
+        }
     }
 }
 
