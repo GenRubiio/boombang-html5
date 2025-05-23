0beeb1d5829c4e0234d5f5608798e583167d4874
evgeny.lyubeznyy <evgeny.lyubeznyy@basetis.com>
2025-05-23 07:21:22 +0200

    refactor: Replace UserSceneResource with UserResource across controllers; enhance UserResource with additional properties

--- server/src/resources/UserResource.js
+++ server/src/resources/UserResource.js
@@ -3,14 +3,22 @@ const Resource = require('./Resource');
 class UserResource extends Resource {
     transform(data) {
         return {
+            id: data.socket.id,
             socket_id: data.socket.id,
             username: data.username,
             is_admin: false,
             is_vip: false,
             avatar_id: data.avatarId,
             gender: "man",
+            x: data.currentAreaPosition ? data.currentAreaPosition.x : null,
+            y: data.currentAreaPosition ? data.currentAreaPosition.y : null,
+            z: data.currentAreaPosition ? data.currentAreaPosition.z : null,
+            avatar_id: data.avatarId,
             uppercuts_send: data.uppercutsSend,
-            uppercuts_received: data.uppercutsReceived
+            uppercuts_received: data.uppercutsReceived,
+            uppercut_level: data.uppercutLevel,
+            uppercut_selected: data.uppercutSelected,
+            animations: [],
         };
     }
 }
