fd5cfe8aa8ff001aea31a98387af3458fba6b132
Evgeny Lyubeznyy <evgeny.lyubeznyy@basetis.com>
2025-07-16 19:52:15 +0200

    feat: add new navigation images and enhance island screen layout with improved scenario buttons

--- client/src/views/screens/game/island/IslandScreen.vue
+++ client/src/views/screens/game/island/IslandScreen.vue
@@ -1,33 +1,71 @@
 <template>
   <div id="island-screen" class="island-screen">
-    <!-- Botones de escenario -->
-    <div class="scenario-buttons">
-      <!-- Botones para escenarios existentes -->
-      <button
-        v-for="scene in sceneData.scenes"
-        :key="scene.id"
-        class="scenario-button"
-        @click="handleSceneAction(scene)"
-      >
-        {{ scene.name }}
-      </button>
+    <div class="island-card">
+      <div class="island__user-name">
+        <h1>{{ sceneData.user.username }}</h1>
+      </div>
+      <div class="island__info">
+        <div class="island__info-name">
+          {{ sceneData.name }}
+        </div>
+        <div class="island__info-description">
+          {{ sceneData.description }}
+        </div>
+        <!-- Botones de escenario -->
+        <div class="scenario-buttons">
+          <div class="scenario-button__list">
+            <div class="scenario-button__list-title">Localizaciones</div>
+          </div>
+          <!-- Botones para escenarios existentes -->
+          <div
+            v-for="scene in sceneData.scenes"
+            :key="scene.id"
+            class="scenario-button__container"
+          >
+            <div class="scenario-button__container-left">
+              <button
+                :key="scene.id"
+                class="scenario-button"
+                @click="handleSceneAction(scene)"
+              >
+                {{ scene.name }}
+              </button>
+            </div>
+            <div class="scenario-button__container-right">
+              <div class="scenario-button__container-right__count">0</div>
+            </div>
+          </div>
 
-      <!-- Botones para añadir nuevos escenarios (solo si es mi isla) -->
-      <template
-        v-if="
-          sceneData.user_id == $socket.user.db_id ||
-          sceneData.userId == $socket.user.db_id
-        "
-      >
-        <button
-          v-for="n in 5 - (sceneData.scenes ? sceneData.scenes.length : 0)"
-          :key="'add-' + n"
-          class="scenario-button"
-          @click="handleSceneAction(null)"
-        >
-          Añadir escenario
-        </button>
-      </template>
+          <!-- Botones para añadir nuevos escenarios (solo si es mi isla) -->
+          <template
+            v-if="
+              sceneData.user_id == $socket.user.db_id ||
+              sceneData.userId == $socket.user.db_id
+            "
+          >
+            <div
+              v-for="n in 5 - (sceneData.scenes ? sceneData.scenes.length : 0)"
+              :key="'add-' + n"
+              class="scenario-button__container"
+            >
+              <div class="scenario-button__container-left">
+                <button
+                  :key="'add-' + n"
+                  class="scenario-button scenario-button--add"
+                  @click="handleSceneAction(null)"
+                >
+                  Añadir escenario
+                </button>
+              </div>
+              <div class="scenario-button__container-right">
+                <div class="scenario-button__container-right__icon">
+                  <i class="las la-angle-double-right"></i>
+                </div>
+              </div>
+            </div>
+          </template>
+        </div>
+      </div>
     </div>
 
     <!-- Imagen de la brújula -->
@@ -142,32 +180,152 @@ export default {
   pointer-events: auto;
 }
 
-.scenario-buttons {
-  position: absolute;
-  top: 20px;
-  left: 50%;
-  transform: translateX(-50%);
+.scenario-button__list {
   display: flex;
-  gap: 10px;
+}
+
+.scenario-button__list-title {
+  font-size: 12px;
+  font-weight: bold;
+  color: #69c7ef;
+  margin-bottom: 0px;
+  padding: 2px 30px 2px 4px;
+  background-color: #3c87b3ad;
+  border-radius: 5px;
+  background-color: #3a4b54c9;
+}
+
+.island-card {
+  position: absolute;
   z-index: 100;
+  width: 260px;
+  top: 20px;
+}
+
+.island__user-name {
+  font-size: 20px;
+  font-weight: bold;
+  color: white;
+  text-align: start;
+  margin-bottom: 10px;
+  background-color: #3c87b3ad;
+  padding: 10px 15px;
+  border-bottom-right-radius: 10px;
+  border-top-right-radius: 10px;
+  border: 5px solid #3c87b3ad;
+  border-left: none;
+}
+
+.island__info {
+  background-color: #3c87b3ad;
+  padding: 10px 15px;
+  border-bottom-right-radius: 10px;
+  border-top-right-radius: 10px;
+  border: 5px solid #3c87b3ad;
+  border-left: none;
+}
+
+.island__info-name {
+  font-size: 18px;
+  font-weight: bold;
+  color: white;
+  margin-bottom: 10px;
+  text-align: start;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  width: 100%;
+}
+
+.island__user-name h1 {
+  margin: 0;
+  padding: 0;
+  font-size: 26px;
+  font-weight: bold;
+  color: white;
+}
+
+.island__info-description {
+  font-size: 14px;
+  color: white;
+  margin-bottom: 10px;
+  text-align: start;
+  background-color: #3a4b54c9;
+  height: 125px;
+  overflow-y: auto;
+  border-radius: 5px;
+}
+
+.scenario-buttons {
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+}
+
+.scenario-button__container {
+  display: flex;
+  gap: 5px;
+}
+
+.scenario-button__container-left {
+  width: 100%;
+}
+
+.scenario-button__container-right {
+  display: flex;
+  gap: 5px;
+}
+
+.scenario-button__container-right__count {
+  background-color: #3a4b54c9;
+  color: white;
+  border-radius: 5px;
+  padding: 5px;
+  text-align: center;
+  font-size: 15px;
+  height: 35px;
+  aspect-ratio: 1 / 1;
+  box-sizing: border-box;
+}
+
+.scenario-button__container-right__icon {
+  background-color: #3a4b54c9;
+  color: white;
+  border-radius: 20px;
+  padding: 5px;
+  text-align: center;
+  font-size: 15px;
+  height: 35px;
+  aspect-ratio: 1 / 1;
+  box-sizing: border-box;
+  color: #3687b3;
 }
 
 .scenario-button {
-  padding: 10px 20px;
-  background-color: #4caf50;
+  background-color: #3a4b54c9;
   color: white;
   border: none;
   border-radius: 5px;
-  cursor: pointer;
-  font-size: 16px;
+  padding: 5px;
+  width: 100%;
+  text-align: left;
+  font-size: 15px;
+  height: 35px;
+  display: inline-flex;
+  align-items: center;
+  transition: background-color 0.3s ease;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+}
+
+.scenario-button--add {
+  color: #3687b3;
 }
 
 .scenario-button:hover {
-  background-color: #45a049;
-  width: 100%;
-  height: 100%;
-  overflow: hidden;
-  pointer-events: auto;
+  background-color: #1c2c35ad;
+  cursor: pointer;
 }
 
 .brujiula {
