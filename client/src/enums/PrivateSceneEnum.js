56bab8f2dd7e18e4d14a9687b1caf59b1f9de90f
Evgeny Lyubeznyy <evgeny.lyubeznyy@basetis.com>
2025-07-19 19:26:02 +0200

    feat: add PrivateScene3 with preload functionality and associated assets

--- client/src/enums/PrivateSceneEnum.js
+++ client/src/enums/PrivateSceneEnum.js
@@ -3,6 +3,7 @@ import Enum from './Enum';
 const PrivateSceneEnum = new Enum({
     SCENE_1: 1,
     SCENE_2: 2,
+    SCENE_3: 3
 });
 
 export default PrivateSceneEnum;
---
+++ client/src/phaser/preloaders/private-scene/PrivateScene3Preload.js
@@ -0,0 +1,60 @@
+import asset_item1_image from "../../../assets/game/private-scenes/3/white/2.png";
+import asset_item2_image from "../../../assets/game/private-scenes/3/white/5.png";
+import asset_item3_image from "../../../assets/game/private-scenes/3/white/8.png";
+import asset_item4_image from "../../../assets/game/private-scenes/3/white/11.png";
+import asset_item5_image from "../../../assets/game/private-scenes/3/white/14.png";
+import asset_item6_image from "../../../assets/game/private-scenes/3/white/17.png";
+import asset_item7_image from "../../../assets/game/private-scenes/3/20.png";
+import asset_item8_image from "../../../assets/game/private-scenes/3/23.png";
+import SceneUtils from "../../../utils/SceneUtils";
+
+class PrivateScene3Preload {
+    static preload(gameScene) {
+        const suffix = '_private_scene_' + gameScene.sceneData.scenery.id;
+        gameScene.load.image("item_1" + suffix, asset_item1_image);
+        gameScene.load.image("item_2" + suffix, asset_item2_image);
+        gameScene.load.image("item_3" + suffix, asset_item3_image);
+        gameScene.load.image("item_4" + suffix, asset_item4_image);
+        gameScene.load.image("item_5" + suffix, asset_item5_image);
+        gameScene.load.image("item_6" + suffix, asset_item6_image);
+        gameScene.load.image("item_7" + suffix, asset_item7_image);
+        gameScene.load.image("item_8" + suffix, asset_item8_image);
+    }
+
+    static load(gameScene) {
+        const suffix = '_private_scene_' + gameScene.sceneData.scenery.id;
+        let items = [
+            { name: "item_1", x: 507, y: 226, custom_depth: 63, show_item: true, show_controller: false }, //Ok
+            { name: "item_2", x: 509, y: 374, custom_depth: 76, show_item: true, show_controller: false }, //Ok
+            { name: "item_3", x: 545, y: 246, custom_depth: 92, show_item: true, show_controller: false }, //Ok
+            { name: "item_4", x: 490, y: 398, custom_depth: 93, show_item: true, show_controller: false }, //Ok
+            { name: "item_5", x: 506, y: 656, custom_depth: 99, show_item: true, show_controller: false }, //Ok
+            { name: "item_6", x: 891, y: 201, custom_depth: 165, show_item: true, show_controller: false }, //Ok
+            { name: "item_7", x: 507, y: 400, custom_depth: 150, show_item: true, show_controller: false }, //Ok
+            { name: "item_8", x: 504, y: 157, custom_depth: 151, show_item: true, show_controller: false } //Ok
+        ];
+
+        items.forEach(item => {
+            this.#loadSingleItem(gameScene, suffix, item);
+        });
+
+        SceneUtils.moveItems(gameScene);
+    }
+
+    static #loadSingleItem(gameScene, suffix, item) {
+        if (!item.show_item) {
+            return; // Si no se debe mostrar el item, no hacemos nada
+        }
+        // Creamos el sprite en (x,y)
+        const sprite = gameScene.add.image(item.x, item.y, item.name + suffix)
+            .setOrigin(0.5, 1)
+            .setDepth(item.custom_depth || item.y)
+            .setName(item.name + suffix);
+        if (item.show_controller) {
+            // Si es un item que se puede mover, añadimos el controlador
+            SceneUtils.moveItem(gameScene, sprite);
+        }
+    }
+}
+
+export default PrivateScene3Preload;
