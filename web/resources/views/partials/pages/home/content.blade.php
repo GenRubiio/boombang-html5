6cf6ff212e82b18946cea04145a6b237f3661356
Evgeny Lyubeznyy <evgeny.lyubeznyy@basetis.com>
2025-07-25 09:59:23 +0200

    feat(home): restructure home page layout and add news section with dynamic content

--- web/resources/views/partials/pages/home/content.blade.php
+++ web/resources/views/partials/pages/home/content.blade.php
@@ -1,7 +1,13 @@
 <div class="container-1200">
     <div class="page-home__content">
-        <div class="page-home__content-title">
-            ÚLTIMAS NOTICIAS
+        <div class="page-home__content__left">
+            <div class="page-home__content__left-title">
+                ÚLTIMAS NOTICIAS
+            </div>
+            @include('partials.pages.home.news')
+        </div>
+        <div class="page-home__content__right">
+
         </div>
     </div>
 </div>
