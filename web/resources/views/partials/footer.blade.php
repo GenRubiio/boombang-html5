68a822f017ec87997776ceca4a42c16d57a77eae
Evgeny Lyubeznyy <evgeny.lyubeznyy@basetis.com>
2025-07-27 13:16:50 +0200

    feat(footer): implement footer layout with social links and tooltips

--- web/resources/views/partials/footer.blade.php
+++ web/resources/views/partials/footer.blade.php
@@ -1,11 +1,49 @@
 <footer>
-    <div class="footer__items-container">
-        <x-footer-items />
-    </div>
-    <div class="footer__info-social-networks-container">
-        <x-footer-social-networks />
-    </div>
-    <div class="footer__legal-container">
-        <x-footer-legal />
+    <div class="container-1200">
+        <div class="footer-container">
+            <div>
+                <div class="footer-container__left">
+                    <div class="footer-container__left__title">
+                        Sigue a BoomMania
+                    </div>
+                    <div class="social-links">
+                        <a href="https://www.facebook.com/boombanggames" target="_blank" class="social-link tooltip"
+                            style="border-color: #002e86; background-color: #0076eb;">
+                            <i class="fab fa-facebook-f"></i>
+                            <span class="tooltiptext">Facebook</span>
+                        </a>
+                        <a href="https://www.instagram.com/boombanggames" target="_blank" class="social-link tooltip"
+                            style="border-color: #f00075; background-color: #ff007a;">
+                            <i class="fab fa-instagram"></i>
+                            <span class="tooltiptext">Instagram</span>
+                        </a>
+                        <a href="https://www.youtube.com/@boombanggames" target="_blank" class="social-link tooltip"
+                            style="border-color: #ff0000; background-color: #ff0000;">
+                            <i class="fab fa-youtube"></i>
+                            <span class="tooltiptext">YouTube</span>
+                        </a>
+                        <a href="https://discord.gg/boombang" target="_blank" class="social-link tooltip"
+                            style="border-color: #5865f2; background-color: #5865f2;">
+                            <i class="fab fa-discord"></i>
+                            <span class="tooltiptext">Discord</span>
+                        </a>
+                        <a href="https://twitter.com/boombanggames" target="_blank" class="social-link tooltip"
+                            style="border-color: #1da1f2; background-color: #1da1f2;">
+                            <i class="fab fa-twitter"></i>
+                            <span class="tooltiptext">Twitter</span>
+                        </a>
+                    </div>
+                </div>
+                <div class="footer-container__right">
+                    <div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-container__bottom">
+                <div>
+                    © BoomMania 2025 Todos los derechos reservados a sus respectivos autores.
+                </div>
+            </div>
+        </div>
     </div>
 </footer>
