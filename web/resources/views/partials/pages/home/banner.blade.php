c4ade6bf503b95d4303fcda51edb52a3556d3bc3
Evgeny Lyubeznyy <evgeny.lyubeznyy@basetis.com>
2025-07-27 12:46:54 +0200

    feat(home): add tooltip functionality and social links to the play button in the banner

--- web/resources/views/partials/pages/home/banner.blade.php
+++ web/resources/views/partials/pages/home/banner.blade.php
@@ -7,9 +7,40 @@
                 <div class="page-home__banner-content__button-avatar">
                     <img src="{{ asset('images/home/bruja-volando.webp') }}" alt="Avatar" class="img-fluid">
                 </div>
-                <a href="{{ makeUrl('Play') }}" class="play-btn open-popup">
-                    Jugar
-                </a>
+                <div class="play-btn-container">
+                    <div>
+                        <a href="{{ makeUrl('Play') }}" class="play-btn open-popup">
+                            Jugar
+                        </a>
+                    </div>
+                    <div class="social-links">
+                        <a href="https://www.facebook.com/boombanggames" target="_blank" class="social-link tooltip"
+                            style="border-color: #002e86; background-color: #0076eb;">
+                            <i class="fab fa-facebook-f"></i>
+                            <span class="tooltiptext">Facebook</span>
+                        </a>
+                        <a href="https://www.instagram.com/boombanggames" target="_blank" class="social-link tooltip"
+                            style="border-color: #f00075; background-color: #ff007a;">
+                            <i class="fab fa-instagram"></i>
+                            <span class="tooltiptext">Instagram</span>
+                        </a>
+                        <a href="https://www.youtube.com/@boombanggames" target="_blank" class="social-link tooltip"
+                            style="border-color: #ff0000; background-color: #ff0000;">
+                            <i class="fab fa-youtube"></i>
+                            <span class="tooltiptext">YouTube</span>
+                        </a>
+                        <a href="https://discord.gg/boombang" target="_blank" class="social-link tooltip"
+                            style="border-color: #5865f2; background-color: #5865f2;">
+                            <i class="fab fa-discord"></i>
+                            <span class="tooltiptext">Discord</span>
+                        </a>
+                        <a href="https://twitter.com/boombanggames" target="_blank" class="social-link tooltip"
+                            style="border-color: #1da1f2; background-color: #1da1f2;">
+                            <i class="fab fa-twitter"></i>
+                            <span class="tooltiptext">Twitter</span>
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
