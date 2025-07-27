3d399c3009f6c7b14fd3b93e0f72369ec3d46aef
Evgeny Lyubeznyy <evgeny.lyubeznyy@basetis.com>
2025-07-27 13:20:36 +0200

    fix(styles): reduce border width and remove border color from social links in footer and banner

--- web/resources/views/partials/footer.blade.php
+++ web/resources/views/partials/footer.blade.php
@@ -8,27 +8,27 @@
                     </div>
                     <div class="social-links">
                         <a href="https://www.facebook.com/boombanggames" target="_blank" class="social-link tooltip"
-                            style="border-color: #002e86; background-color: #0076eb;">
+                            style="background-color: #0076eb;">
                             <i class="fab fa-facebook-f"></i>
                             <span class="tooltiptext">Facebook</span>
                         </a>
                         <a href="https://www.instagram.com/boombanggames" target="_blank" class="social-link tooltip"
-                            style="border-color: #f00075; background-color: #ff007a;">
+                            style="background-color: #ff007a;">
                             <i class="fab fa-instagram"></i>
                             <span class="tooltiptext">Instagram</span>
                         </a>
                         <a href="https://www.youtube.com/@boombanggames" target="_blank" class="social-link tooltip"
-                            style="border-color: #ff0000; background-color: #ff0000;">
+                            style="background-color: #ff0000;">
                             <i class="fab fa-youtube"></i>
                             <span class="tooltiptext">YouTube</span>
                         </a>
                         <a href="https://discord.gg/boombang" target="_blank" class="social-link tooltip"
-                            style="border-color: #5865f2; background-color: #5865f2;">
+                            style="background-color: #5865f2;">
                             <i class="fab fa-discord"></i>
                             <span class="tooltiptext">Discord</span>
                         </a>
                         <a href="https://twitter.com/boombanggames" target="_blank" class="social-link tooltip"
-                            style="border-color: #1da1f2; background-color: #1da1f2;">
+                            style="background-color: #1da1f2;">
                             <i class="fab fa-twitter"></i>
                             <span class="tooltiptext">Twitter</span>
                         </a>
