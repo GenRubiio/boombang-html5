524db9cd7f1e5f53af8c519081afdc8e60c8a39d
Evgeny Lyubeznyy <evgeny.lyubeznyy@basetis.com>
2025-08-03 13:16:25 +0200

    fix: update 'uploads' disk configuration to include visibility and permissions

--- web/config/filesystems.php
+++ web/config/filesystems.php
@@ -39,16 +39,27 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
             'throw' => false,
         ],
 
         // used for Backpack/CRUD
+        // used for Backpack/CRUD
         'uploads' => [
             'driver' => 'local',
             'root' => public_path(),
-            'url' => env('APP_URL'),
+            'visibility' => 'public',
+            'permissions' => [
+                'file' => [
+                    'public'  => 0644,
+                    'private' => 0600,
+                ],
+                'dir' => [
+                    'public'  => 0755,
+                    'private' => 0700,
+                ],
+            ],
         ],
 
         // used for Backpack/BackupManager
