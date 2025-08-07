3617ea3f438f7c1541022296c94271a70826fef9
Evgeny Lyubeznyy <evgeny.lyubeznyy@basetis.com>
2025-08-07 14:45:48 +0200

    fix: simplify meta tags logic in page-metas.blade.php

--- web/resources/views/partials/page-metas.blade.php
+++ web/resources/views/partials/page-metas.blade.php
@@ -1,10 +1,10 @@
-@section('title'){{ (!is_array($object) && (isset($object->page_title) && empty($object->page_title)) ? $object->title . ' | ' . config('settings.name') : !is_array($object)) ? $object->page_title : '' }}@endsection
-@section('meta-title'){{ !is_array($object) && isset($object->meta_title) ? $object->meta_title : '' }}@endsection
-@section('meta-description'){{ !is_array($object) && isset($object->meta_description) ? $object->meta_description : '' }}@endsection
-@section('meta-keywords'){{ !is_array($object) && isset($object->meta_keywords) ? $object->meta_keywords : '' }}@endsection
-@section('og-title'){{ !is_array($object) && isset($object->og_title) ? $object->og_title : '' }}@endsection
-@section('og-description'){{ !is_array($object) && isset($object->og_description) ? $object->og_description : '' }}@endsection
-@section('og-image'){{ !is_array($object) && isset($object->og_image) ? $object->og_image : '' }}@endsection
-@section('tw-title'){{ !is_array($object) && isset($object->tw_title) ? $object->tw_title : '' }}@endsection
-@section('tw-description'){{ !is_array($object) && isset($object->tw_description) ? $object->tw_description : '' }}@endsection
-@section('tw-image'){{ !is_array($object) && isset($object->tw_image) ? $object->tw_image : '' }}@endsection
+@section('title'){{ $object->title ?? '' }}@endsection
+@section('meta-title'){{ $object->meta_title ?? '' }}@endsection
+@section('meta-description'){{ $object->meta_description ?? '' }}@endsection
+@section('meta-keywords'){{ $object->meta_keywords ?? '' }}@endsection
+@section('og-title'){{ $object->og_title ?? '' }}@endsection
+@section('og-description'){{ $object->og_description ?? '' }}@endsection
+@section('og-image'){{ $object->og_image ?? '' }}@endsection
+@section('tw-title'){{ $object->tw_title ?? '' }}@endsection
+@section('tw-description'){{ $object->tw_description ?? '' }}@endsection
+@section('tw-image'){{ $object->tw_image ?? '' }}@endsection
