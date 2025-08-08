6e6e950fe6368ba149b904c6466942d247e110b5
Evgeny Lyubeznyy <evgeny.lyubeznyy@basetis.com>
2025-08-08 12:28:16 +0200

    feat: update social media image sections to use default logo if not provided

--- web/resources/views/partials/page-metas-seos.blade.php
+++ web/resources/views/partials/page-metas-seos.blade.php
@@ -4,7 +4,7 @@
 @section('meta-keywords'){{ !is_array($object) && isset($object->meta_keywords) ? $object->meta_keywords : '' }}@endsection
 @section('og-title'){{ !is_array($object) && isset($object->og_title) ? $object->og_title : '' }}@endsection
 @section('og-description'){{ !is_array($object) && isset($object->og_description) ? $object->og_description : '' }}@endsection
-@section('og-image'){{ !is_array($object) && isset($object->og_image) ? $object->og_image : '' }}@endsection
+@section('og-image'){{ !is_array($object) && isset($object->og_image) ? $object->og_image : asset(config('settings.logo')) }}@endsection
 @section('tw-title'){{ !is_array($object) && isset($object->tw_title) ? $object->tw_title : '' }}@endsection
 @section('tw-description'){{ !is_array($object) && isset($object->tw_description) ? $object->tw_description : '' }}@endsection
-@section('tw-image'){{ !is_array($object) && isset($object->tw_image) ? $object->tw_image : '' }}@endsection
+@section('tw-image'){{ !is_array($object) && isset($object->tw_image) ? $object->tw_image : asset(config('settings.logo')) }}@endsection
--- web/resources/views/partials/page-metas.blade.php
+++ web/resources/views/partials/page-metas.blade.php
@@ -4,7 +4,7 @@
 @section('meta-keywords'){{ $object->meta_keywords ?? '' }}@endsection
 @section('og-title'){{ $object->og_title ?? '' }}@endsection
 @section('og-description'){{ $object->og_description ?? '' }}@endsection
-@section('og-image'){{ $object->og_image ?? '' }}@endsection
+@section('og-image'){{ empty($object->og_image) ? asset(config('settings.logo')) : $object->og_image }}@endsection
 @section('tw-title'){{ $object->tw_title ?? '' }}@endsection
 @section('tw-description'){{ $object->tw_description ?? '' }}@endsection
-@section('tw-image'){{ $object->tw_image ?? '' }}@endsection
+@section('tw-image'){{ empty($object->tw_image) ? asset(config('settings.logo')) : $object->tw_image }}@endsection
