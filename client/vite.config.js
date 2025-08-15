8fe6ca0b8dd247e65fcb8596a3107082f003cc7c
Evgeny Lyubeznyy <evgeny.lyubeznyy@basetis.com>
2025-08-15 09:51:17 +0200

    feat(EmojisPickerComponent.vue): refactor emoji categories to load from external JSON file

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,4 +8,9 @@ export default defineConfig({
     allowedHosts: ['play.boommania.com']
   },
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
