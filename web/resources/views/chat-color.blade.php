3f3211525982699a9fe0417be01014f2321c2c26
Evgeny Lyubeznyy <evgeny.lyubeznyy@basetis.com>
2025-08-16 14:18:59 +0200

    fix(chat-color.blade.php): update background color and text color for improved visibility

--- web/resources/views/chat-color.blade.php
+++ web/resources/views/chat-color.blade.php
@@ -1,4 +1,4 @@
-<div>
+<div style="width: 100%; height: 100vh; background-color: #000; padding-top: 50px; padding-left: 50px;">
     <div
         style="display: flex;
     align-items: center;
@@ -8,16 +8,16 @@
     width: 365px;
     border-radius: 5px;
     position: relative;
-    background-color: #ffd700;
+    background-color: #420143;
     padding: 4px 5px;">
         <div style="position: absolute;">
             <img style=" width: 20px;" src="{{ url('images/cara_media.svg') }}" alt="Description of image">
         </div>
         <div
-            style="font-family: Arial, Helvetica, sans-serif; color: #000; font-weight: bold; font-size: 14px; margin-left: 23px;margin-top: 2px;">
+            style="font-family: Arial, Helvetica, sans-serif; color: #fff; font-weight: bold; font-size: 14px; margin-left: 23px;margin-top: 2px;">
             God:
         </div>
-        <div style="font-family: Arial, Helvetica, sans-serif; color: #000; font-weight: normal; font-size: 14px;">
+        <div style="font-family: Arial, Helvetica, sans-serif; color: #fff; font-weight: normal; font-size: 14px;">
             Hello BoomMania 😀😱🤣🫠
         </div>
     </div>
