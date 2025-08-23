da152e8ed8e077ccf8f1881bd6a687e9870aabca
Evgeny Lyubeznyy <evgeny.lyubeznyy@basetis.com>
2025-08-23 14:02:28 +0200

    feat: Add CatalogItemTypesEnum, CatalogItemResource, and CatalogCategoryResource with migration updates for catalog items

--- api/app/Http/Controllers/Api/Game/Catalog/LobbyGachaApiController.php
+++ api/app/Http/Controllers/Api/Game/Catalog/LobbyGachaApiController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\Game\Catalog;
 
 use Exception;
+use App\Models\CatalogItem;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Resources\Json\JsonResource;
@@ -15,9 +16,18 @@ class LobbyGachaApiController extends Controller
     public function items(Request $request): JsonResource
     {
         try {
+            $items = CatalogItem::where('in_lobby_gacha', true)
+                ->where('is_active', true)
+                ->get();
+
+            $items = [];
+            foreach ($items as $item) {
+                $items[$item->type][$item->stars][] = $item;
+            }
+
             return $this->successResponse(
                 [
-    
+                    'items' => $items
                 ]
             );
         } catch (Exception $e) {
---
+++ api/app/Http/Resources/CatalogItemResource.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use App\Http\Resources\Traits\DtoResourceTrait;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CatalogItemResource extends JsonResource
+{
+    use DtoResourceTrait;
+
+    private static $data;
+
+    public function __construct($resource)
+    {
+        parent::__construct($resource);
+        self::$data = $resource;
+    }
+
+    public function toArray(Request $request): array
+    {
+        $return = [
+            'id' => $this->id,
+            'category_id' => $this->category_id,
+            'name' => $this->name,
+            'sprite_name' => $this->sprite_name,
+            'image' => $this->image,
+            'description' => $this->description,
+            'price' => $this->price,
+            'price_type' => $this->price_type,
+            'discount' => $this->discount,
+            'stars' => $this->stars,
+        ];
+
+        if (debug_backtrace()[1]['function'] == "toDTO") {
+            if ($this->relationLoaded('category')) {
+                $return['category'] = new CatalogCategoryResource($this->whenLoaded('category'));
+            }
+        } else {
+            $return['category'] = new CatalogCategoryResource($this->whenLoaded('category'));
+        }
+
+        return $return;
+    }
+}
