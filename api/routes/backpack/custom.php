b2526734a60cceae29e873715cdd807f286f0a67
Evgeny Lyubeznyy <evgeny.lyubeznyy@basetis.com>
2025-08-27 14:39:34 +0200

    refactor: Remove user-related CRUD controllers and requests, and update user model relationships

--- api/routes/backpack/custom.php
+++ api/routes/backpack/custom.php
@@ -28,10 +28,6 @@ Route::group([
     Route::crud('ranking-category', 'RankingCategoryCrudController');
     Route::crud('ranking', 'RankingCrudController');
     Route::crud('v-ranking-summary', 'VRankingSummaryCrudController');
-    Route::crud('user-ficha', 'UserFichaCrudController');
-    Route::crud('user-chat', 'UserChatCrudController');
-    Route::crud('user-colorname', 'UserColornameCrudController');
-    Route::crud('user-shadow', 'UserShadowCrudController');
 }); // this should be the absolute last line of this file
 
 /**
