de4dfa2364fb2e15922ca6b6b7234381065de98e
Evgeny Lyubeznyy <evgeny.lyubeznyy@basetis.com>
2025-09-08 10:40:56 +0200

    feat: Update base API URL handling in PublicSceneLoader and PublicSceneModel, and include it in PublicSceneResource transformation

--- server/src/models/PublicSceneModel.js
+++ server/src/models/PublicSceneModel.js
@@ -8,6 +8,7 @@ class PublicSceneModel extends SceneModel {
         this.uuid = uuid; // UUID único de la escena
         this.max_users = row.max_users; // Número máximo de usuarios permitidos en el área
         this.scene_type = SceneTypesEnum.PUBLIC_SCENE;// Tipo de modelo
+        this.base_api_url = row.base_api_url ? row.base_api_url : ''; // URL base para los recursos
         this.assets_data = row.assets_data ? row.assets_data : []; // Datos de los assets en formato JSON
         this.npc = row.npc ? new NpcModel(row.npc) : null; // Datos del NPC asociado a la escena
     }
