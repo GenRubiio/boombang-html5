3802b11d570ae6f08d02e54f1d28ea82da2bee0d
Evgeny Lyubeznyy <evgeny.lyubeznyy@basetis.com>
2025-09-12 12:50:51 +0200

    feat: add dotenv configuration for environment variables and update main window settings

--- launcher/main.js
+++ launcher/main.js
@@ -1,11 +1,17 @@
+require('dotenv').config();
 const { app, BrowserWindow, globalShortcut } = require('electron');
+const path = require('path');
 
 let mainWindow;
 
 app.whenReady().then(() => {
   mainWindow = new BrowserWindow({
-    width: 1200,
-    height: 800,
+    width: 1015,
+    height: 683,
+    resizable: false,
+    title: 'BoomMania | Game Launcher',
+    autoHideMenuBar: true,
+    icon: path.join(__dirname, 'assets/favicon.ico'),
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: true,
@@ -13,8 +19,18 @@ app.whenReady().then(() => {
   });
 
   // Cargar la URL donde está el frontend de Vue
-  const vueURL = "http://localhost:5173/"; // Cambia esto si tu Vue está en otro servidor
+  const vueURL = process.env.VUE_URL || "https://play.boommania.com/";
   mainWindow.loadURL(vueURL);
+
+  mainWindow.webContents.on('page-title-updated', (event) => {
+    event.preventDefault();
+    mainWindow.setTitle('BoomMania | Game Launcher');
+  });
+
+  // Registrar atajo de teclado F1 para abrir DevTools
+  globalShortcut.register('F1', () => {
+    if (mainWindow) mainWindow.webContents.toggleDevTools();
+  });
 
   // Registrar atajo de teclado F5 para recargar la página
   globalShortcut.register('F5', () => {
