e43313c39011978dfbe38aa2b28393222051fdb4
Evgeny Lyubeznyy <evgeny.lyubeznyy@basetis.com>
2025-09-18 08:16:47 +0200

    Add new zombie and boomer spritesheets for enhanced animations
    - Added new spritesheets for zombie animations (spritesheet-0.webp, spritesheet-1.webp).
    - Added additional spritesheets for boomer animations (spritesheet-3.webp to spritesheet-7.webp).
    - Updated AvatarBoomerLoad.js to include new boomer spritesheets in the loading process.
    - Updated AvatarZombieLoad.js to include new zombie spritesheets in the loading process.

--- client/src/phaser/load/avatars/AvatarBoomerLoad.js
+++ client/src/phaser/load/avatars/AvatarBoomerLoad.js
@@ -1,6 +1,11 @@
 import asset_spritesheet0_image from '@/assets/game/avatars/boomer/animations/spritesheet-0.webp';
 import asset_spritesheet1_image from '@/assets/game/avatars/boomer/animations/spritesheet-1.webp';
 import asset_spritesheet2_image from '@/assets/game/avatars/boomer/animations/spritesheet-2.webp';
+import asset_spritesheet3_image from '@/assets/game/avatars/boomer/animations/spritesheet-3.webp';
+import asset_spritesheet4_image from '@/assets/game/avatars/boomer/animations/spritesheet-4.webp';
+import asset_spritesheet5_image from '@/assets/game/avatars/boomer/animations/spritesheet-5.webp';
+import asset_spritesheet6_image from '@/assets/game/avatars/boomer/animations/spritesheet-6.webp';
+import asset_spritesheet7_image from '@/assets/game/avatars/boomer/animations/spritesheet-7.webp';
 import asset_atlas_json from '@/assets/game/avatars/boomer/animations/atlas.json';
 import asset_cara_peque_image from '@/assets/game/avatars/boomer/cara_peque.svg';
 import asset_cara_media_image from '@/assets/game/avatars/boomer/cara_media.svg';
@@ -10,7 +15,12 @@ class AvatarBoomerLoad {
         const webpFiles = [
             asset_spritesheet0_image,
             asset_spritesheet1_image,
-            asset_spritesheet2_image
+            asset_spritesheet2_image,
+            asset_spritesheet3_image,
+            asset_spritesheet4_image,
+            asset_spritesheet5_image,
+            asset_spritesheet6_image,
+            asset_spritesheet7_image,
         ];
 
         asset_atlas_json.textures.forEach((texture, i) => {
--- client/src/phaser/load/avatars/AvatarZombieLoad.js
+++ client/src/phaser/load/avatars/AvatarZombieLoad.js
@@ -1,12 +1,23 @@
 import asset_spritesheet0_image from '@/assets/game/avatars/zombie/animations/spritesheet-0.webp';
 import asset_spritesheet1_image from '@/assets/game/avatars/zombie/animations/spritesheet-1.webp';
+import asset_spritesheet2_image from '@/assets/game/avatars/zombie/animations/spritesheet-2.webp';
+import asset_spritesheet3_image from '@/assets/game/avatars/zombie/animations/spritesheet-3.webp';
+import asset_spritesheet4_image from '@/assets/game/avatars/zombie/animations/spritesheet-4.webp';
+import asset_spritesheet5_image from '@/assets/game/avatars/zombie/animations/spritesheet-5.webp';
 import asset_atlas_json from '@/assets/game/avatars/zombie/animations/atlas.json';
 import asset_cara_peque_image from '@/assets/game/avatars/zombie/cara_peque.svg';
 import asset_cara_media_image from '@/assets/game/avatars/zombie/cara_media.svg';
 
 class AvatarZombieLoad {
     static main(gameScene, avatarId) {
-        const webpFiles = [asset_spritesheet0_image, asset_spritesheet1_image];
+        const webpFiles = [
+            asset_spritesheet0_image, 
+            asset_spritesheet1_image, 
+            asset_spritesheet2_image, 
+            asset_spritesheet3_image, 
+            asset_spritesheet4_image, 
+            asset_spritesheet5_image
+        ];
 
         asset_atlas_json.textures.forEach((texture, i) => {
             texture.image = webpFiles[i];
