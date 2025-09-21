aef6f245ceb97f6e3b7afcff6aebaf1a47cced46
Evgeny Lyubeznyy <evgeny.lyubeznyy@basetis.com>
2025-09-21 12:43:16 +0200

    feat: add scheduled command for queue processing in console routes

--- api/routes/console.php
+++ api/routes/console.php
@@ -3,8 +3,18 @@
 use Illuminate\Foundation\Console\ClosureCommand;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     /** @var ClosureCommand $this */
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+/**
+ * Cola:
+ * En producción es mejor un supervisor (supervisor, systemd, etc.).
+ * Si quieres mantener el comportamiento anterior, puedes dejar este schedule:
+ */
+Schedule::command('queue:work --stop-when-empty')
+    ->everyMinute()
+    ->withoutOverlapping();
