1db32694d33ed20f6fe7669fda72e61a757b6640
GrigorPogosyan <grigorycbb@gmail.com>
2025-10-26 09:43:56 +0100

    Add new spritesheets and update Brujita animations
    - Added new spritesheets (0 to 3) for Brujita character animations.
    - Updated Brujita's config.json to adjust position values for various animations.
    - Incremented asset version in cacheManagerConfig.js to 1.1.2.
    - Changed AnimationEditorController to use Brujita's avatar configuration.
    - Extended AvatarBrujitaLoad to include additional spritesheets (4 to 12).
    - Adjusted margins in UiElementsComponent.vue for improved layout.

--- client/src/config/cacheManagerConfig.js
+++ client/src/config/cacheManagerConfig.js
@@ -15,7 +15,7 @@ const cacheConfig = {
      * Cambia este valor cuando quieras invalidar el caché existente
      * @type {string}
      */
-    assetVersion: '1.1.0'
+    assetVersion: '1.1.2'
 };
 
 export default cacheConfig;
